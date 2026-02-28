1860abecd2612c6177f99e3b0003a26717351f31
AlaBhs <alabelhadjsalem@gmail.com>
2026-02-28 03:41:17 +0100

    feat(infrastructure): add EF Core setup with AppDbContext and implement RequestRepository with EF Core

---
+++ ErrandsManagement.Infrastructure/Persistence/Repositories/RequestRepository.cs
@@ -0,0 +1,27 @@
+﻿using ErrandsManagement.Application.Common.Interfaces;
+using ErrandsManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErrandsManagement.Infrastructure.Persistence.Repositories;
+
+public sealed class RequestRepository : IRequestRepository
+{
+    private readonly AppDbContext _context;
+
+    public RequestRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Request request, CancellationToken cancellationToken)
+    {
+        await _context.Requests.AddAsync(request, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<Request?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _context.Requests
+            .FirstOrDefaultAsync(r => r.Id == id, cancellationToken);
+    }
+}
