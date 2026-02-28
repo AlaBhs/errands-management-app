6446a689ef0455eb9b0e5002d98f4258fe9904b9
AlaBhs <alabelhadjsalem@gmail.com>
2026-02-28 17:13:13 +0100

    feat(infrastructure): integrate EF Core, configure entity mappings, and apply initial migration

---
+++ ErrandsManagement.Infrastructure/Data/Configurations/AssignmentConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ErrandsManagement.Domain.Entities;
+
+namespace ErrandsManagement.Infrastructure.Persistence.Configurations;
+
+public class AssignmentConfiguration : IEntityTypeConfiguration<Assignment>
+{
+    public void Configure(EntityTypeBuilder<Assignment> builder)
+    {
+        builder.Property(x => x.ActualCost)
+               .HasPrecision(18, 2);
+    }
+}
---
+++ ErrandsManagement.Infrastructure/Data/Configurations/RequestConfiguration.cs
@@ -0,0 +1,57 @@
+﻿using ErrandsManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ErrandsManagement.Infrastructure.Data.Configurations;
+
+public sealed class RequestConfiguration : IEntityTypeConfiguration<Request>
+{
+    public void Configure(EntityTypeBuilder<Request> builder)
+    {
+        builder.HasKey(r => r.Id);
+
+        builder.Property(r => r.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(r => r.Description)
+            .IsRequired();
+
+        builder.Property(r => r.RequesterId)
+            .IsRequired();
+
+        builder.Property(r => r.Priority)
+            .IsRequired();
+
+        builder.Property(r => r.Status)
+            .IsRequired();
+
+        builder.Property(x => x.EstimatedCost)
+               .HasPrecision(18, 2);
+
+        builder.Property(r => r.Deadline);
+
+        // Owned Value Object
+        builder.OwnsOne(r => r.DeliveryAddress, address =>
+        {
+            address.Property(a => a.Street)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            address.Property(a => a.City)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            address.Property(a => a.PostalCode)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            address.Property(a => a.Country)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            address.Property(a => a.Note)
+                .HasMaxLength(500);
+        });
+    }
+}
