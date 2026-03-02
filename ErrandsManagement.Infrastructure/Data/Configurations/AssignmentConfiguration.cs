4fcb9ed0d86bc0e6c30dff6da9305676373c2fb0
AlaBhs <alabelhadjsalem@gmail.com>
2026-03-02 21:28:17 +0100

    fix(infrastructure): correct EF Core tracking and key generation

--- ErrandsManagement.Infrastructure/Data/Configurations/AssignmentConfiguration.cs
+++ ErrandsManagement.Infrastructure/Data/Configurations/AssignmentConfiguration.cs
@@ -8,7 +8,18 @@ public class AssignmentConfiguration : IEntityTypeConfiguration<Assignment>
 {
     public void Configure(EntityTypeBuilder<Assignment> builder)
     {
-        builder.Property(x => x.ActualCost)
+        builder.HasKey(a => a.Id);
+
+        builder.Property(a => a.Id)
+       .ValueGeneratedNever();
+
+        builder.Property(a => a.RequestId)
+               .IsRequired();
+
+        builder.Property(a => a.CourierId)
+               .IsRequired();
+
+        builder.Property(a => a.ActualCost)
                .HasPrecision(18, 2);
     }
 }
