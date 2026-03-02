e725d8f67b1000de56921347757480106ba7522a
AlaBhs <alabelhadjsalem@gmail.com>
2026-03-02 21:35:51 +0100

    feat(application): add custom Exception handling and debugging methods in repo

--- ErrandsManagement.Application/Interfaces/IRequestRepository.cs
+++ ErrandsManagement.Application/Interfaces/IRequestRepository.cs
@@ -4,7 +4,14 @@ namespace ErrandsManagement.Application.Common.Interfaces;
 
 public interface IRequestRepository
 {
+    Task SaveChangesAsync(CancellationToken cancellationToken);
     Task AddAsync(Request request, CancellationToken cancellationToken);
     Task<Request?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
     Task<List<Request>> GetAllAsync(CancellationToken cancellationToken);
+
+    // ============================ Temporary debug methods ============================
+    //Task<bool> ExistsAsync(Guid id, CancellationToken cancellationToken);
+    //string GetEntityState(object entity);
+    //void MarkAsAdded<T>(T entity) where T : class;
+    // ============================ Temporary debug methods ============================
 }
