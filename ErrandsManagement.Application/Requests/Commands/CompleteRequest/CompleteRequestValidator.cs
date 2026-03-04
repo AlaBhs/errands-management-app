b00721c0ffdac0ad67c1ebe1e9024d229242046e
AlaBhs <alabelhadjsalem@gmail.com>
2026-03-04 05:00:20 +0100

    feat(validation): add validators, validation behavior, and structured 400 responses for commands using FluentValidation

---
+++ ErrandsManagement.Application/Requests/Commands/CompleteRequest/CompleteRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace ErrandsManagement.Application.Requests.Commands.CompleteRequest;
+
+
+public sealed class CompleteRequestValidator
+    : AbstractValidator<CompleteRequestCommand>
+{
+    public CompleteRequestValidator()
+    {
+        RuleFor(x => x.RequestId)
+            .NotEmpty();
+
+        RuleFor(x => x.ActualCost)
+            .GreaterThanOrEqualTo(0)
+            .When(x => x.ActualCost.HasValue);
+
+        RuleFor(x => x.Note)
+            .MaximumLength(500)
+            .When(x => x.Note != null);
+    }
+}
