a2fb0966939f41957be835e07f1a51d82ce7c159
AlaBhs <alabelhadjsalem@gmail.com>
2026-03-05 01:04:43 +0100

    chore(testing): initialize test projects for domain, application and infrastructure layers

---
+++ tests/ErrandsManagement.Application.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ErrandsManagement.Application.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/ErrandsManagement.Domain.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ErrandsManagement.Domain.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
