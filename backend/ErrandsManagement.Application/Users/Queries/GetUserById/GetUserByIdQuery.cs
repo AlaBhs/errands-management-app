ef6b7dc0123afd44a41b396ab215bbc5c7f4dc5b
AlaBhs <alabelhadjsalem@gmail.com>
2026-03-13 04:57:12 +0100

    feat(users) : add GetUserById query

---
+++ backend/ErrandsManagement.Application/Users/Queries/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using ErrandsManagement.Application.Users.DTOs;
+using MediatR;
+
+namespace ErrandsManagement.Application.Users.Queries.GetUserById;
+
+public sealed record GetUserByIdQuery(Guid UserId) : IRequest<UserListItemDto>;
