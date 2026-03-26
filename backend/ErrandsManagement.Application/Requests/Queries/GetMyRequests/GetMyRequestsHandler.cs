e5a56f0b763d5726580e91211462523e7d8f5d1a
AlaBhs <alabelhadjsalem@gmail.com>
2026-03-26 17:06:31 +0100

    feat(requests): remove unused imports in GetMyRequestsHandler

--- backend/ErrandsManagement.Application/Requests/Queries/GetMyRequests/GetMyRequestsHandler.cs
+++ backend/ErrandsManagement.Application/Requests/Queries/GetMyRequests/GetMyRequestsHandler.cs
@@ -1,8 +1,6 @@
 ﻿using ErrandsManagement.Application.Common.Pagination;
-using ErrandsManagement.Application.DTOs;
 using ErrandsManagement.Application.Interfaces;
 using ErrandsManagement.Application.Requests.DTOs;
-using ErrandsManagement.Application.Requests.Queries.GetAllRequests;
 using MediatR;
 
 namespace ErrandsManagement.Application.Requests.Queries.GetMyRequests;
