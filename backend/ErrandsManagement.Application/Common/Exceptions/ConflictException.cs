91f3ba8ff6e39e827014f919643c3713b434dff8
AlaBhs <alabelhadjsalem@gmail.com>
2026-04-25 01:05:51 +0100

    feat(request-templates): add ConflictException to shared application exceptions

---
+++ backend/ErrandsManagement.Application/Common/Exceptions/ConflictException.cs
@@ -0,0 +1,6 @@
+﻿namespace ErrandsManagement.Application.Common.Exceptions;
+
+public sealed class ConflictException : ApplicationException
+{
+    public ConflictException(string message) : base(message) { }
+}
