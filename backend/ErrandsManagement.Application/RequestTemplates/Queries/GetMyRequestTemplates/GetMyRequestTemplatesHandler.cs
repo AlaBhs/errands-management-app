45189a0f5360991ee441aeb390f456da0d5523e3
AlaBhs <alabelhadjsalem@gmail.com>
2026-04-25 01:21:19 +0100

    feat(request-templates): add GetMyRequestTemplates and GetRequestTemplateById queries

---
+++ backend/ErrandsManagement.Application/RequestTemplates/Queries/GetMyRequestTemplates/GetMyRequestTemplatesHandler.cs
@@ -0,0 +1,27 @@
+﻿using ErrandsManagement.Application.Common.Pagination;
+using ErrandsManagement.Application.Interfaces;
+using ErrandsManagement.Application.RequestTemplates.DTOs;
+using MediatR;
+
+namespace ErrandsManagement.Application.RequestTemplates.Queries.GetMyRequestTemplates;
+
+public sealed class GetMyRequestTemplatesHandler
+    : IRequestHandler<GetMyRequestTemplatesQuery, PagedResult<RequestTemplateListItemDto>>
+{
+    private readonly IRequestTemplateRepository _repository;
+
+    public GetMyRequestTemplatesHandler(IRequestTemplateRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<PagedResult<RequestTemplateListItemDto>> Handle(
+        GetMyRequestTemplatesQuery query,
+        CancellationToken cancellationToken)
+    {
+        return await _repository.GetPagedByUserAsync(
+            query.UserId,
+            query.Parameters,
+            cancellationToken);
+    }
+}
---
+++ backend/ErrandsManagement.Application/RequestTemplates/Queries/GetRequestTemplateById/GetRequestTemplateByIdHandler.cs
@@ -0,0 +1,47 @@
+﻿using ErrandsManagement.Application.Common.Exceptions;
+using ErrandsManagement.Application.Interfaces;
+using ErrandsManagement.Application.Requests.DTOs;
+using ErrandsManagement.Application.RequestTemplates.DTOs;
+using ErrandsManagement.Domain.Entities;
+using MediatR;
+
+namespace ErrandsManagement.Application.RequestTemplates.Queries.GetRequestTemplateById;
+
+public sealed class GetRequestTemplateByIdHandler
+    : IRequestHandler<GetRequestTemplateByIdQuery, RequestTemplateDetailsDto>
+{
+    private readonly IRequestTemplateRepository _repository;
+
+    public GetRequestTemplateByIdHandler(IRequestTemplateRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<RequestTemplateDetailsDto> Handle(
+        GetRequestTemplateByIdQuery query,
+        CancellationToken cancellationToken)
+    {
+        var template = await _repository.GetByIdAsync(query.TemplateId, cancellationToken)
+            ?? throw new NotFoundException(nameof(RequestTemplate), query.TemplateId);
+
+        if (template.CreatedBy != query.UserId)
+            throw new ForbiddenAccessException("You do not have access to this template.");
+
+        return new RequestTemplateDetailsDto(
+            template.Id,
+            template.Name,
+            template.Title,
+            template.Description,
+            template.Category.ToString(),
+            new AddressDto(
+                template.Address.City,
+                template.Address.PostalCode,
+                template.Address.Country,
+                template.Address.Street,
+                template.Address.Note,
+                template.Address.Latitude,
+                template.Address.Longitude),
+            template.EstimatedCost,
+            template.CreatedAt);
+    }
+}
