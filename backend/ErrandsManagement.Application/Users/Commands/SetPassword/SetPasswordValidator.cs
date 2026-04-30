5abda37889d077d70ccec08ddf7f5737f0d11afd
AlaBhs <alabelhadjsalem@gmail.com>
2026-04-30 10:08:37 +0100

    feat(user-onboarding): SetPasswordCommand (token-based activation)

---
+++ backend/ErrandsManagement.Application/Users/Commands/SetPassword/SetPasswordValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace ErrandsManagement.Application.Users.Commands.SetPassword;
+
+public sealed class SetPasswordValidator : AbstractValidator<SetPasswordCommand>
+{
+    public SetPasswordValidator()
+    {
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.Token).NotEmpty();
+        RuleFor(x => x.NewPassword).NotEmpty().MinimumLength(8);
+        RuleFor(x => x.ConfirmPassword)
+            .Equal(x => x.NewPassword)
+            .WithMessage("Passwords do not match.");
+    }
+}
